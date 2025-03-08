14a3ce009e065774aa84010e4995462e4bc9c826
iamisabellams <isabellamedeirosreal@gmail.com>
2025-03-07 21:46:41 -0300

    Add files via upload

---
+++ aula03_tarefa04.py
@@ -0,0 +1,23 @@
+x=10 
+print(f"O valor inicial é de : {x}")
+
+x+=5
+print(f"O resultado da soma é de : {x}")
+
+x-=3
+print(f"O resultado da subtração é de : {x}")
+
+x*-2
+print(f"O resultado da multiplicação é de : {x}")
+
+x/=4
+print(f"O resultado da divisão é de : {x}")
+
+x//=3
+print(f"O resultado da divisão inteira é de : {x}")
+
+x%=2
+print(f"O resultado do modresto é de : {x}")
+
+x**=3
+print(f"O resultado é da exponenciação é de : {x}")
