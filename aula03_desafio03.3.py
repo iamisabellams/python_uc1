b3bc50ebc5b38b8cedb72f51b92b3727d43c1a6c
iamisabellams <isabellamedeirosreal@gmail.com>
2025-03-07 21:16:20 -0300

    Add files via upload

---
+++ aula03_desafio03.3.py
@@ -0,0 +1,4 @@
+celsius  = float(input("Digite a temperaturaem ºC : "))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"A temperatura em ºF é : {fahrenheit}")
---
+++ aula03_tarefa02.py
@@ -0,0 +1,21 @@
+a=float(input("Digite o 1º número : "))
+b=float(input("Digite o 2º número : "))
+
+soma=a+b
+subtração=a-b
+multiplicação=a*b
+divisão_real=a/b
+divisão_inteira=a//b
+módulo=a%b
+exponenciação=a**b
+
+#print("O resultado da soma é ")
+#print(soma)
+
+print(f"O resultado da soma é {soma}")
+print(f"O resultado da soma é {subtração}")
+print(f"O resultado da soma é {multiplicação}")
+print(f"O resultado da soma é {divisão_real}")
+print(f"O resultado da soma é {divisão_inteira}")
+print(f"O resultado da soma é {módulo}")
+print(f"O resultado da soma é {exponenciação}")
